d4e4ecc745a18068c184adaf7f66b23fad9eb9cf
codewreaker <israel.agyeman.prempeh@gmail.com>
2026-01-07 19:25:46 +0000

    chore(feat): modding to a monorepo fewer services

--- apps/web/src/components/grid-table/Header.tsx
+++ apps/web/src/components/grid-table/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
+import { Button } from '@dwellpass/ui';
 import type { MenuItem } from './index';
 
 interface HeaderProps {
